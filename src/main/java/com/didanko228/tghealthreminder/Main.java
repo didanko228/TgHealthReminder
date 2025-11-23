6f3d92bf55de8ce2ae1f3a8185a4e288c7a8ff21
”didanko228” <artemdidanov072@gmail.com>
2025-11-23 20:35:15 +0300

    simply echo bot added Config (.env) added dependencies for work project added Logger

---
+++ src/main/java/com/didanko228/tghealthreminder/Main.java
@@ -0,0 +1,22 @@
+package com.didanko228.tghealthreminder;
+
+import com.didanko228.tghealthreminder.utils.Config;
+import com.didanko228.tghealthreminder.utils.Logger;
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+
+public class Main {
+    public static String PROJECT_NAME = "TgHealthReminder";
+    public static String PROJECT_ID = PROJECT_NAME.toLowerCase();
+
+    static void main() {
+        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+            botsApplication.registerBot(Config.BOT_TOKEN, new TgHealthReminder(Config.BOT_TOKEN));
+
+            Logger.info("Started!");
+
+            Thread.currentThread().join();
+        } catch (Exception e) {
+            Logger.error("Error for start bot", e);
+        }
+    }
+}
--- src/main/java/com/didanko228/tghealthreminder/TgHealthReminder.java
+++ src/main/java/com/didanko228/tghealthreminder/TgHealthReminder.java
@@ -1,7 +1,41 @@
 package com.didanko228.tghealthreminder;
 
-public class TgHealthReminder {
-    static void main() {
-        System.out.println("Hello World!");
+import com.didanko228.tghealthreminder.utils.Logger;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.message.Message;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+public class TgHealthReminder implements LongPollingSingleThreadUpdateConsumer {
+    private final TelegramClient telegramClient;
+
+    public TgHealthReminder(String botToken) {
+        telegramClient = new OkHttpTelegramClient(botToken);
+    }
+
+    @Override
+    public void consume(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            Message msg = update.getMessage();
+
+            String text = msg.getText();
+            long chat_id = msg.getChatId();
+
+            SendMessage message = SendMessage
+                    .builder()
+                    .chatId(chat_id)
+                    .text(text)
+                    .build();
+
+            try {
+                telegramClient.execute(message);
+            } catch (TelegramApiException e) {
+                Logger.error("Error", e);
+            }
+        }
     }
 }
