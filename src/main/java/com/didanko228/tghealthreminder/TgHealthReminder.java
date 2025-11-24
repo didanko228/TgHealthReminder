50d6fc2f743cbd0635e70ec32de1f60567ed7040
”didanko228” <artemdidanov072@gmail.com>
2025-11-24 18:42:51 +0300

    add package handlers add InlineKeyboard in msg

--- src/main/java/com/didanko228/tghealthreminder/TgHealthReminder.java
+++ src/main/java/com/didanko228/tghealthreminder/TgHealthReminder.java
@@ -1,12 +1,9 @@
 package com.didanko228.tghealthreminder;
 
-import com.didanko228.tghealthreminder.utils.Logger;
+import com.didanko228.tghealthreminder.handlers.TextHandler;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.message.Message;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
 public class TgHealthReminder implements LongPollingSingleThreadUpdateConsumer {
@@ -20,22 +17,7 @@ public class TgHealthReminder implements LongPollingSingleThreadUpdateConsumer {
     public void consume(Update update) {
         // We check if the update has a message and the message has text
         if (update.hasMessage() && update.getMessage().hasText()) {
-            Message msg = update.getMessage();
-
-            String text = msg.getText();
-            long chat_id = msg.getChatId();
-
-            SendMessage message = SendMessage
-                    .builder()
-                    .chatId(chat_id)
-                    .text(text)
-                    .build();
-
-            try {
-                telegramClient.execute(message);
-            } catch (TelegramApiException e) {
-                Logger.error("Error", e);
-            }
+            TextHandler.handle(telegramClient, update.getMessage());
         }
     }
 }
---
+++ src/main/java/com/didanko228/tghealthreminder/handlers/TextHandler.java
@@ -0,0 +1,37 @@
+package com.didanko228.tghealthreminder.handlers;
+
+import com.didanko228.tghealthreminder.utils.Logger;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.message.Message;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+public class TextHandler {
+    public static void handle(TelegramClient telegramClient, Message msg) {
+        String text = msg.getText();
+        long chat_id = msg.getChatId();
+
+        SendMessage message = SendMessage
+                .builder()
+                .chatId(chat_id)
+                .text(text)
+                .replyMarkup(InlineKeyboardMarkup
+                        .builder()
+                        .keyboardRow(new InlineKeyboardRow(InlineKeyboardButton
+                                .builder()
+                                .text("test")
+                                .callbackData("test")
+                                .build()))
+                        .build())
+                .build();
+
+        try {
+            telegramClient.execute(message);
+        } catch (TelegramApiException e) {
+            Logger.error("Error sending message", e);
+        }
+    }
+}
